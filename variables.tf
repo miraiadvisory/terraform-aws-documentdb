785d17882e0b620de457b02e98a44889235cfa6c
Miguel Garcia <miguel.garcia@bi-geek.com>
2020-02-06 16:26:51 +0100

    removing terraform 0.11 syntax deprecated

--- variables.tf
+++ variables.tf
@@ -1,23 +1,23 @@
 variable "projectname" {
   default = ""
-  type    = "string"
+  type    = string
 }
 
 variable "environment" {
   default = ""
-  type    = "string"
+  type    = string
 }
 
 variable "vpc" {
   description = "The ID of the VPC where will be created"
   default     = ""
-  type        = "string"
+  type        = string
 }
 
 variable "vpn_sg" {
   description = "The ID of the VPN server"
   default     = ""
-  type        = "string"
+  type        = string
 }
 
 variable "eb_nodesSecuritygroup" {
@@ -29,66 +29,66 @@ variable "eb_nodesSecuritygroup" {
 variable "priv-subnet-1" {
   description = "Private Subnet 1 ID"
   default     = ""
-  type        = "string"
+  type        = string
 }
 
 variable "priv-subnet-2" {
   description = "Private Subnet 2 ID"
   default     = ""
-  type        = "string"
+  type        = string
 }
 
 variable "priv-subnet-3" {
   description = "Private Subnet 3 ID"
   default     = ""
-  type        = "string"
+  type        = string
 }
 
 variable "docdb_familyversion" {
   description = "Version to launch on documentdb (MONGODB compatibility)"
   default     = "docdb3.6"
-  type        = "string"
+  type        = string
 }
 
 variable "docdb_tlsEnable" {
   description = "Set TLS communication to/from DocumentDB cluster"
   default     = "false"
-  type        = "string"
+  type        = string
 }
 
 variable "docdb_profilerEnable" {
   description = "Set mongodb profiler on/off"
   default     = "false"
-  type        = "string"
+  type        = string
 }
 
 variable "docdb_masterPassword" {
   description = "Password for DocumentDB admin"
   default     = "changeme"
-  type        = "string"
+  type        = string
 }
 
 variable "docdb_port" {
   description = "Port where DocumentDB will be accesed"
   default     = "27017"
-  type        = "string"
+  type        = string
 }
 
 variable "docdb_storageEncrypted" {
   description = "Set DocumentDB storage encryption at rest"
   default     = "true"
-  type        = "string"
+  type        = string
 }
 
 variable "docdb_instanceNumber" {
   description = "Number of instances to launch within the cluster"
   default     = "1"
-  type        = "string"
+  type        = string
 
 }
 
 variable "docdb_instanceType" {
   description = "Type of instance to launch into DocumentDB cluster"
   default     = "db.r5.large"
-  type        = "string"
+  type        = string
 }
